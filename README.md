8686b92528fafec6d2ee7a2ea1d8ad61198195ab
Valentine Briese <valentinegb@icloud.com>
2024-09-02 20:53:47 -0700

    Add `java_home` setting

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Zed Java
 
 This extension adds support for the [Java](https://github.com/zed-extensions/java) language.
+
+## Configuration
+
+You can optionally configure the Java home that JDTLS (the language server) uses like so:
+
+```json
+{
+  "lsp": {
+    "jdtls": {
+      "settings": {
+        "java_home": "/path/to/jdk"
+      }
+    }
+  }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,31 @@ impl zed::Extension for Java {
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &zed::LanguageServerId,
+        language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> zed::Result<zed::Command> {
+        let java_home =
+            zed::settings::LspSettings::for_worktree(language_server_id.as_ref(), worktree)?
+                .settings
+                .and_then(|settings| {
+                    settings.get("java_home").and_then(|java_home_value| {
+                        java_home_value
+                            .as_str()
+                            .and_then(|java_home_str| Some(java_home_str.to_string()))
+                    })
+                });
+        let mut env = Vec::new();
+
+        if let Some(java_home) = java_home {
+            env.push(("JAVA_HOME".to_string(), java_home));
+        }
+
         Ok(zed::Command {
             command: worktree
                 .which("jdtls")
                 .ok_or("could not find JDTLS in PATH")?,
-            args: vec![],
-            env: vec![],
+            args: Vec::new(),
+            env,
         })
     }
 }
