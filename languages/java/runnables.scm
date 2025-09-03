0aa632412b5470443dec31a417b842f741b97ba8
lxgr-linux <lxgr@protonmail.com>
2025-09-03 09:43:52 +0200

    feat(#45): Added runnable tests

--- languages/java/runnables.scm
+++ languages/java/runnables.scm
@@ -14,3 +14,46 @@
     ) @_
     (#set! tag java-main)
 )
+
+; Run the test function
+(
+    (package_declaration
+        (scoped_identifier) @java_package_name
+    )
+    (class_declaration
+        name: (identifier) @java_class_name
+        body: (class_body
+            (method_declaration
+                (modifiers
+                    (marker_annotation
+                        name: (identifier) @annotation_name
+                    )
+                )
+                name: (identifier) @run @java_method_name
+                (#eq? @annotation_name "Test")
+            )
+        )
+    ) @_
+    (#set! tag java-test-method)
+)
+
+; Run the test function
+(
+    (package_declaration
+        (scoped_identifier) @java_package_name
+    )
+    (class_declaration
+        name: (identifier) @java_class_name @run
+        body: (class_body
+            (method_declaration
+                (modifiers
+                    (marker_annotation
+                        name: (identifier) @annotation_name
+                    )
+                )
+                (#eq? @annotation_name "Test")
+            )
+        )
+    ) @_
+    (#set! tag java-test-class)
+)
