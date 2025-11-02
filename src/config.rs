0e5cac6b094491091ce8ad388b0b9c3750182ba4
Karl-Erik Enkelmann <110300169+playdohface@users.noreply.github.com>
2025-11-02 16:09:08 +0100

    Simplify configuration and docs (#115)
    Closes #99
     
    - add top-level configuration values for lombok_support and java_home
    - change lombok_support default to `true`
    - overhaul the `README`

--- src/config.rs
+++ src/config.rs
@@ -5,14 +5,18 @@ use crate::util::expand_home_path;
 pub fn get_java_home(configuration: &Option<Value>, worktree: &Worktree) -> Option<String> {
     // try to read the value from settings
     if let Some(configuration) = configuration
-        && let Some(java_home) = configuration.pointer("/java/home").and_then(|x| x.as_str()) {
-            match expand_home_path(worktree, java_home.to_string()) {
-                Ok(home_path) => return Some(home_path),
-                Err(err) => {
-                    println!("{}", err);
-                }
-            };
-        }
+        && let Some(java_home) = configuration
+            .pointer("/java_home")
+            .or_else(|| configuration.pointer("/java/home")) // legacy support
+            .and_then(|x| x.as_str())
+    {
+        match expand_home_path(worktree, java_home.to_string()) {
+            Ok(home_path) => return Some(home_path),
+            Err(err) => {
+                println!("{}", err);
+            }
+        };
+    }
 
     // try to read the value from env
     match worktree
@@ -30,8 +34,9 @@ pub fn is_lombok_enabled(configuration: &Option<Value>) -> bool {
         .as_ref()
         .and_then(|configuration| {
             configuration
-                .pointer("/java/jdt/ls/lombokSupport/enabled")
+                .pointer("/lombok_support")
+                .or_else(|| configuration.pointer("/java/jdt/ls/lombokSupport/enabled")) // legacy support
                 .and_then(|enabled| enabled.as_bool())
         })
-        .unwrap_or(false)
+        .unwrap_or(true)
 }
