ca1eff66d7efdbc4aa5b86f2d456a92dcc1467c2
Alejandro Cardona <alejo.98k@gmail.com>
2025-11-23 01:27:22 -0500

    Add outline improvements

--- languages/java/outline.scm
+++ languages/java/outline.scm
@@ -10,23 +10,11 @@
             "final"
             "strictfp"
             "static"
-        ]* @context)
+        ]* @context)?
     "class" @context
     name: (_) @name) @item
 
 (record_declaration
-    (modifiers
-        [
-            "private"
-            "public"
-            "protected"
-            "abstract"
-            "sealed"
-            "non-sealed"
-            "final"
-            "strictfp"
-            "static"
-        ]* @context)
     "record" @context
     name: (_) @name) @item
 
@@ -41,9 +29,38 @@
             "non-sealed"
             "strictfp"
             "static"
-        ]* @context)
+        ]* @context)?
     "interface" @context
     name: (_) @name) @item
+
+(enum_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+            "static"
+            "final"
+            "strictfp"
+        ]* @context)?
+    "enum" @context
+    name: (_) @name) @item
+
+(annotation_type_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+            "abstract"
+            "static"
+            "strictfp"
+        ]* @context)?
+    "@interface" @context
+    name: (_) @name) @item
+
+(enum_constant
+    name: (identifier) @name) @item
 
 (method_declaration
     (modifiers
@@ -57,11 +74,87 @@
             "native"
             "strictfp"
             "synchronized"
-        ]* @context)
+        ]* @context)?
+    type: (_) @context
     name: (_) @name
     parameters: (formal_parameters
       "(" @context
-      ")" @context)) @item
+      (formal_parameter
+        type: (_) @context)?
+      ("," @context
+        (formal_parameter
+          type: (_) @context)?)*
+      ")" @context)
+    body: (_) @item)
+
+(method_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+            "abstract"
+            "static"
+            "final"
+            "native"
+            "strictfp"
+            "synchronized"
+        ]* @context)?
+    type: (_) @context
+    name: (_) @name
+    parameters: (formal_parameters
+      "(" @context
+      (formal_parameter
+        type: (_) @context)?
+      ("," @context
+        (formal_parameter
+          type: (_) @context)?)*
+      ")" @context)
+    ";"  @item)
+
+(constructor_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+            "static"
+            "final"
+        ]* @context)?
+    name: (_) @name
+    parameters: (formal_parameters
+      "(" @context
+      (formal_parameter
+        type: (_) @context)?
+      ("," @context
+        (formal_parameter
+          type: (_) @context)?)*
+      ")" @context)
+    body: (_) @item)
+
+(compact_constructor_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+        ]* @context)?
+    name: (_) @name
+    body: (_) @item)
+
+(annotation_type_element_declaration
+    (modifiers
+        [
+            "private"
+            "public"
+            "protected"
+            "abstract"
+            "static"
+        ]* @context)?
+    type: (_) @context
+    name: (_) @name
+    "(" @context
+    ")" @context) @item
 
 (field_declaration
     (modifiers
@@ -73,6 +166,28 @@
             "final"
             "transient"
             "volatile"
-        ]* @context)
+        ]* @context)?
+    type: (_) @context
     declarator: (variable_declarator
         name: (_) @name)) @item
+
+(constant_declaration
+    (modifiers
+        [
+            "public"
+            "static"
+            "final"
+        ]* @context)?
+    type: (_) @context
+    declarator: (variable_declarator
+        name: (_) @name)) @item
+
+(static_initializer
+    "static" @context
+    (block) @item)
+
+(record_declaration
+    (formal_parameters
+        (formal_parameter
+            type: (_) @context
+            name: (_) @name @item)))
