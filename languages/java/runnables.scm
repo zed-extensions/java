4c8b9035661622c78841d8d267a9c032358de533
Riccardo Strina <85676009+tartarughina@users.noreply.github.com>
2026-03-24 21:58:58 +0100

    Add formatting for tree-sitter query files (#216)
    - Integrate ts_query_ls formatter into the fmt task
    - Create new lint task combining fmt and clippy

--- languages/java/runnables.scm
+++ languages/java/runnables.scm
@@ -1,163 +1,115 @@
 ; Run the main function
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )?
-    (class_declaration
-        (modifiers) @class-modifier
-        (#match? @class-modifier "public")
-        name: (identifier) @java_class_name
-        body: (class_body
-            (method_declaration
-                (modifiers) @modifier
-                name: (identifier) @run
-                (#eq? @run "main")
-                (#match? @modifier "public")
-                (#match? @modifier "static")
-            )
-        )
-    ) @_
-    (#set! tag java-main)
-)
+((package_declaration
+  (scoped_identifier) @java_package_name)?
+  (class_declaration
+    (modifiers) @class-modifier
+    (#match? @class-modifier "public")
+    name: (identifier) @java_class_name
+    body: (class_body
+      (method_declaration
+        (modifiers) @modifier
+        name: (identifier) @run
+        (#eq? @run "main")
+        (#match? @modifier "public")
+        (#match? @modifier "static")))) @_
+  (#set! tag java-main))
 
 ; Run the main class
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )?
-    (class_declaration
-        (modifiers) @class-modifier
-        (#match? @class-modifier "public")
-        name: (identifier) @java_class_name @run
-        body: (class_body
-            (method_declaration
-                (modifiers) @modifier
-                name: (identifier) @method_name
-                (#eq? @method_name "main")
-                (#match? @modifier "public")
-                (#match? @modifier "static")
-            )
-        )
-    ) @_
-    (#set! tag java-main)
-)
+((package_declaration
+  (scoped_identifier) @java_package_name)?
+  (class_declaration
+    (modifiers) @class-modifier
+    (#match? @class-modifier "public")
+    name: (identifier) @java_class_name @run
+    body: (class_body
+      (method_declaration
+        (modifiers) @modifier
+        name: (identifier) @method_name
+        (#eq? @method_name "main")
+        (#match? @modifier "public")
+        (#match? @modifier "static")))) @_
+  (#set! tag java-main))
 
 ; Run test function (marker annotation, e.g. @Test)
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )
-    (class_declaration
-        name: (identifier) @java_class_name
-        body: (class_body
-            (method_declaration
-                (modifiers
-                    [(marker_annotation
-                        name: (identifier) @annotation_name
-                    )
-                    (annotation
-                        name: (identifier) @annotation_name
-                    )]
-                )
-                name: (identifier) @run @java_method_name
-                (#match? @annotation_name "Test$")
-            )
-        )
-    ) @_
-    (#set! tag java-test-method)
-)
+((package_declaration
+  (scoped_identifier) @java_package_name)
+  (class_declaration
+    name: (identifier) @java_class_name
+    body: (class_body
+      (method_declaration
+        (modifiers
+          [
+            (marker_annotation
+              name: (identifier) @annotation_name)
+            (annotation
+              name: (identifier) @annotation_name)
+          ])
+        name: (identifier) @run @java_method_name
+        (#match? @annotation_name "Test$")))) @_
+  (#set! tag java-test-method))
 
 ; Run nested test function
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )
-    (class_declaration
-        name: (identifier) @java_outer_class_name
+((package_declaration
+  (scoped_identifier) @java_package_name)
+  (class_declaration
+    name: (identifier) @java_outer_class_name
+    body: (class_body
+      (class_declaration
+        (modifiers
+          (marker_annotation
+            name: (identifier) @nested_annotation))
+        name: (identifier) @java_class_name
         body: (class_body
-            (class_declaration
-                (modifiers
-                    (marker_annotation
-                        name: (identifier) @nested_annotation
-                    )
-                )
-                name: (identifier) @java_class_name
-                body: (class_body
-                    (method_declaration
-                        (modifiers
-                            [(marker_annotation
-                                name: (identifier) @annotation_name
-                                )
-                            (annotation
-                                name: (identifier) @annotation_name
-                                )]
-                            )
-                        name: (identifier) @run @java_method_name
-                        (#match? @annotation_name "Test$")
-                    )
-                )
-                (#eq? @nested_annotation "Nested")
-            ) @_
-        )
-    )
-    (#set! tag java-test-method-nested)
-)
+          (method_declaration
+            (modifiers
+              [
+                (marker_annotation
+                  name: (identifier) @annotation_name)
+                (annotation
+                  name: (identifier) @annotation_name)
+              ])
+            name: (identifier) @run @java_method_name
+            (#match? @annotation_name "Test$")))
+        (#eq? @nested_annotation "Nested")) @_))
+  (#set! tag java-test-method-nested))
 
 ; Run test class
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )
-    (class_declaration
-        name: (identifier) @java_class_name @run
-        body: (class_body
-            (method_declaration
-                (modifiers
-                    [(marker_annotation
-                        name: (identifier) @annotation_name
-                    )
-                    (annotation
-                        name: (identifier) @annotation_name
-                    )]
-                )
-                (#match? @annotation_name "Test$")
-            )
-        )
-    ) @_
-    (#set! tag java-test-class)
-)
+((package_declaration
+  (scoped_identifier) @java_package_name)
+  (class_declaration
+    name: (identifier) @java_class_name @run
+    body: (class_body
+      (method_declaration
+        (modifiers
+          [
+            (marker_annotation
+              name: (identifier) @annotation_name)
+            (annotation
+              name: (identifier) @annotation_name)
+          ])
+        (#match? @annotation_name "Test$")))) @_
+  (#set! tag java-test-class))
 
 ; Run nested test class
-(
-    (package_declaration
-        (scoped_identifier) @java_package_name
-    )
-    (class_declaration
-        name: (identifier) @java_outer_class_name
+((package_declaration
+  (scoped_identifier) @java_package_name)
+  (class_declaration
+    name: (identifier) @java_outer_class_name
+    body: (class_body
+      (class_declaration
+        (modifiers
+          (marker_annotation
+            name: (identifier) @nested_annotation))
+        name: (identifier) @run @java_class_name
         body: (class_body
-            (class_declaration
-                (modifiers
-                    (marker_annotation
-                        name: (identifier) @nested_annotation
-                    )
-                )
-                name: (identifier) @run @java_class_name
-                body: (class_body
-                    (method_declaration
-                        (modifiers
-                            [(marker_annotation
-                                name: (identifier) @annotation_name
-                                )
-                            (annotation
-                                name: (identifier) @annotation_name
-                                )]
-                            )
-                        (#match? @annotation_name "Test$")
-                    )
-                )
-                (#eq? @nested_annotation "Nested")
-            ) @_
-        )
-    )
-    (#set! tag java-test-class-nested)
-)
+          (method_declaration
+            (modifiers
+              [
+                (marker_annotation
+                  name: (identifier) @annotation_name)
+                (annotation
+                  name: (identifier) @annotation_name)
+              ])
+            (#match? @annotation_name "Test$")))
+        (#eq? @nested_annotation "Nested")) @_))
+  (#set! tag java-test-class-nested))
--- languages/java/textobjects.scm
+++ languages/java/textobjects.scm
@@ -1,65 +1,79 @@
 ; methods
 (method_declaration) @function.around
+
 (method_declaration
   body: (block
-    "{" (_)* @function.inside "}"
-  ))
+    "{"
+    (_)* @function.inside
+    "}"))
 
 ; constructors
 (constructor_declaration) @function.around
+
 (constructor_declaration
   body: (constructor_body
-    "{" (_)* @function.inside "}"
-  ))
+    "{"
+    (_)* @function.inside
+    "}"))
 
 ; lambdas
 (lambda_expression) @function.around
+
 (lambda_expression
   body: (block
-    "{" (_)* @function.inside "}"
-  ))
+    "{"
+    (_)* @function.inside
+    "}"))
+
 (lambda_expression
   body: (_) @function.inside)
 
-
 ; classes
 (class_declaration) @class.around
+
 (class_declaration
   body: (class_body
-    "{" (_)* @class.inside "}"
-  ))
+    "{"
+    (_)* @class.inside
+    "}"))
 
 ; interfaces
 (interface_declaration) @class.around
+
 (interface_declaration
   body: (interface_body
-    "{" (_)* @class.inside "}"
-  ))
+    "{"
+    (_)* @class.inside
+    "}"))
 
 ; enums
 (enum_declaration) @class.around
+
 (enum_declaration
   body: (enum_body
     "{"
     _* @class.inside
-    "}"
-  ))
+    "}"))
 
 ; records
 (record_declaration) @class.around
+
 (record_declaration
   body: (class_body
-    "{" (_)* @class.inside "}"
-  ))
+    "{"
+    (_)* @class.inside
+    "}"))
 
 ; annotations
 (annotation_type_declaration) @class.around
+
 (annotation_type_declaration
   (annotation_type_body
-    "{" (_)* @class.inside "}"
-  ))
-
+    "{"
+    (_)* @class.inside
+    "}"))
 
 ; comments
 ((line_comment)+) @comment.around
+
 (block_comment) @comment.around
